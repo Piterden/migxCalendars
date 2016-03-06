c605d0430ba149ee75196236aeef1e352e02ed1b
Piterden <efremov.a.denis@gmail.com>
2016-03-06 22:52:35 +0300

    Create default.inc.php

---
+++ core/components/migxcalendars/lexicon/ru/default.inc.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * migxcalendars
+ *
+ * @package migxcalendars
+ * @language en
+ */
+
+$_lang['migxcal.movetothrash_ifexits'] = 'Move Events to Thrash if not free in this Category';
+$_lang['migxcal.on_double_events'] = 'On double events in this category';
+$_lang['migxcal.publish'] = 'Публиковать';
+$_lang['migxcal.to_thrash'] = 'В корзину';
+$_lang['migxcal.unpublish'] = 'Снять с публикации';
+$_lang['migxcal.ignore'] = 'Игнорировать';
+$_lang['migxcal.assigned_categories'] = 'Дополнительные категории';
+$_lang['migxcal.category'] = 'Категория';
+$_lang['migxcal.allday'] = 'Целый день';
+$_lang['migxcal.unpublish_dates'] = 'Снять даты с публикации';
+$_lang['migxcal.ondates_in_assigned_cats'] = 'К датам назначенных категорий';
+$_lang['migxcal.yes'] = 'Да';
+$_lang['migxcal.no'] = 'Нет';
+$_lang['migxcal.inherited'] = 'Унаследовано';
+$_lang['migxcal.repeat_weekly'] = 'Раз в неделю';
+$_lang['migxcal.lastdate'] = 'Последнее событие';
+$_lang['migxcal.repeatings'] = 'Повторения событий';
+$_lang['migxcal.event'] = 'Событие';
+$_lang['migxcal.title'] = 'Название';
+$_lang['migxcal.start'] = 'Начало';
+$_lang['migxcal.end'] = 'Конец';
+$_lang['migxcal.start_firstdate'] = 'Начало (первое событие)';
+$_lang['migxcal.end_firstdate'] = 'Конец (первое событие)';
+$_lang['migxcal.singledates'] = 'Дополнительные события';
+$_lang['migxcal.categoryname'] = 'Название категории';
+$_lang['migxcal.background_color'] = 'Цвет фона';
+$_lang['migxcal.border_color'] = 'Цвет бордюра';
+$_lang['migxcal.text_color'] = 'Цвет текста';
+$_lang['migxcal.add_event'] = 'Добавить событие';
+$_lang['migxcal.events'] = 'События';
+$_lang['migxcal.events_cmp_desc'] = 'Управление событиями и группами событий';
+$_lang['migxcal.add_category'] = 'Добавить категорию';
+$_lang['migxcal.categories'] = 'Категории';
+$_lang['migxcal.categories_cmp_desc'] = 'Управление категориями';
+$_lang['migxcal.events_cmp_main_caption'] = 'Менеджер событий';
+$_lang['migxcal.event_active'] = 'Событие активно';
+$_lang['migxcal.active'] = 'Вкл';
+$_lang['migxcal.inactive'] = 'Выкл';
+$_lang['migxcal.active_filter'] = 'Вкл/Выкл';
+$_lang['migxcal.check_availability'] = 'Проверка доступности';
+$_lang['migxcal.availability'] = 'Доступность';
+$_lang['migxcal.available'] = 'Доступно';
+$_lang['migxcal.category_filter'] = 'Фильтр по категориям';
+$_lang['migxcal.warn_move_repeat_2other_day'] = 'Изменение дня пересоздаст все повторяющиеся даты!';
+$_lang['migxcal.warn_change_repeating'] = 'Статус повторений был изменен. Это сотрет все вложенные даты!';
+$_lang['migxcal.warn_move_repeat_2other_week'] = 'Нельзя переносить повторения на другую неделю!';
+$_lang['migxcal.warn_dragnew_repeatingdate'] = 'Нельзя перетаскивать даты на события с повторениями';
+$_lang['migxcal.createon'] = 'Создано в';
+$_lang['migxcal.search'] = 'Поиск';
+$_lang['migxcal.load_datecontainer'] = 'Загрузить групповое событие';
+$_lang['migxcal.hide_published'] = 'Скрыть опубликованные';
+$_lang['migxcal.show_unpublished'] = 'Показать неопубликованные';
+$_lang['migxcal.date_containers'] = 'Группы событий';
+$_lang['migxcal.date_container'] = 'Группа событий';
+$_lang['migxcal.date_removed'] = 'Событие удалено';
+$_lang['migxcal.this_date'] = 'Первое событие';
+$_lang['migxcal.delete_date'] = 'Удалить событие';
+$_lang['migxcal.repeatings_start'] = 'Начало повторения событий';
+$_lang['migxcal.repeatings_end'] = 'Конец повторения событий';
+$_lang['migxcal.recall_datecontainer'] = 'Отозвать группу';
+$_lang['migxcal.delete_datecontainer'] = 'Поместить группу в корзину';
+$_lang['migxcal.remove_date'] = 'Удалить событие';
+$_lang['migxcal.remove_datecontainer'] = 'Удалить группу';
+$_lang['migxcal.description'] = 'Описание';
+$_lang['migxcal.content'] = 'Содержание';
